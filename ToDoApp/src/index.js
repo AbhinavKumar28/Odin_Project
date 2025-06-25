7464b93a0868c496199f04c10e4fbe10ddc8c6ae
ABHINAV KUMAR <emailofabhinav@gmail.com>
2025-06-25 10:45:04 +0530

    If applied, this commit will show basic ui of Todo items.

--- ToDoApp/src/index.js
+++ ToDoApp/src/index.js
@@ -1,2 +1,4 @@
 import "./style.css";
+import func from "./todo/view/todos-view.js"
 console.log("To-Do App is running!");
+func()
---
+++ ToDoApp/src/todo/view/todos-view.js
@@ -0,0 +1,27 @@
+import data from "../../data/data.js"
+export default function todoView(){
+    const mainDiv = document.getElementById("entry-point")
+    data().forEach((element,i) => {
+        const div_for_each_item = document.createElement("div")
+        div_for_each_item.id=`todo-item-${i}`
+        
+        div_for_each_item.classList.add("todoitem__container")
+        const del_for_each_item = document.createElement("button")
+        del_for_each_item.id=`todo-del-${i}`
+        del_for_each_item.textContent="Delete"
+        del_for_each_item.classList.add("todoitem__deletebtn")
+        const span_for_each_item = document.createElement("span")
+        span_for_each_item.id=`todo-span-${i}`
+        span_for_each_item.textContent=element
+        span_for_each_item.classList.add("todoitem__title")
+        const open_for_each_item = document.createElement("button")
+        open_for_each_item.id=`todo-open-${i}`
+        open_for_each_item.textContent="Open"
+        open_for_each_item.classList.add("todoitem__openbtn")
+        
+        div_for_each_item.append(del_for_each_item,span_for_each_item,open_for_each_item)
+        // console.log(a.id)
+        mainDiv.appendChild(div_for_each_item)
+    })
+}
+// todoView()
